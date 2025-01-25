bc725d54716314035a22a116d94469e305e55996
Frank Krick <frank.krick@gmail.com>
2025-01-25 12:56:48 -0500

    started traktor z1 support

---
+++ src/applications/traktor_z1_router.cpp
@@ -0,0 +1,224 @@
+#include <oscpp/client.hpp>
+
+#include <linux/input.h>
+
+#include <fstream>
+#include <iostream>
+#include <ranges>
+#include <string>
+#include <thread>
+
+#include <pwcpp/filter/app_builder.h>
+
+#include <boost/lockfree/spsc_queue.hpp>
+
+struct traktor_z1_message {
+  std::uint16_t gain_l;
+  std::uint16_t hi_l;
+  std::uint16_t mid_l;
+  std::uint16_t low_l;
+  std::uint16_t filter_l;
+  std::uint16_t gain_r;
+  std::uint16_t hi_r;
+  std::uint16_t mid_r;
+  std::uint16_t low_r;
+  std::uint16_t filter_r;
+  std::uint16_t cue_mix;
+  std::uint16_t fader_l;
+  std::uint16_t fader_r;
+  std::uint16_t cross_fader;
+};
+
+int main(int argc, char *argv[]) {
+  std::string device = "/dev/hidraw8";
+
+  if (argc > 1)
+    device = argv[1];
+
+  boost::lockfree::spsc_queue<traktor_z1_message> queue(128);
+
+  traktor_z1_message previous;
+  previous.gain_l = 0;
+  previous.hi_l = 0;
+  previous.mid_l = 0;
+  previous.low_l = 0;
+  previous.filter_l = 0;
+  previous.gain_r = 0;
+  previous.hi_r = 0;
+  previous.mid_r = 0;
+  previous.low_r = 0;
+  previous.filter_r = 0;
+  previous.cue_mix = 0;
+  previous.fader_l = 0;
+  previous.fader_r = 0;
+  previous.cross_fader = 0;
+  bool is_init(true);
+
+  pwcpp::filter::AppBuilder<std::nullptr_t> builder;
+  builder.set_filter_name("pmx-midi-router")
+      .set_media_type("Osc")
+      .set_media_class("Osc/Source")
+      .add_arguments(argc, argv)
+      .add_output_port("osc", "8 bit raw midi")
+      .add_signal_processor([&queue, &previous, &is_init](
+                                auto position, auto &in_ports, auto &out_ports,
+                                auto &user_data, auto &parameters) {
+        auto buffer = out_ports[0]->get_buffer();
+        if (buffer.has_value()) {
+          if (queue.read_available() > 0) {
+            traktor_z1_message message;
+            while (queue.pop(message)) {
+              if (is_init) {
+                previous = message;
+                is_init = false;
+              } else {
+                std::array<
+                    std::optional<std::tuple<std::string, std::uint16_t>>, 14>
+                    changes{};
+
+                if (previous.gain_l != message.gain_l) {
+                  changes[0] = std::make_tuple("/gain_l", message.gain_l);
+                }
+
+                if (previous.hi_l != message.hi_l) {
+                  changes[1] = std::make_tuple("/hi_l", message.hi_l);
+                }
+
+                if (previous.mid_l != message.mid_l) {
+                  changes[2] = std::make_tuple("/mid_l", message.mid_l);
+                }
+
+                if (previous.low_l != message.low_l) {
+                  changes[3] = std::make_tuple("/low_l", message.low_l);
+                }
+
+                if (previous.filter_l != message.filter_l) {
+                  changes[4] = std::make_tuple("/filter_l", message.filter_l);
+                }
+
+                if (previous.gain_r != message.gain_r) {
+                  changes[5] = std::make_tuple("/gain_r", message.gain_r);
+                }
+
+                if (previous.hi_r != message.hi_r) {
+                  changes[6] = std::make_tuple("/hi_r", message.hi_r);
+                }
+
+                if (previous.mid_r != message.mid_r) {
+                  changes[7] = std::make_tuple("/mid_r", message.mid_r);
+                }
+
+                if (previous.low_r != message.low_r) {
+                  changes[8] = std::make_tuple("/low_r", message.low_r);
+                }
+
+                if (previous.filter_r != message.filter_r) {
+                  changes[9] = std::make_tuple("/filter_r", message.filter_r);
+                }
+
+                if (previous.cue_mix != message.cue_mix) {
+                  changes[10] = std::make_tuple("/cue_mix", message.cue_mix);
+                }
+
+                if (previous.fader_l != message.fader_l) {
+                  changes[11] = std::make_tuple("/fader_l", message.fader_l);
+                }
+
+                if (previous.fader_r != message.fader_r) {
+                  changes[12] = std::make_tuple("/fader_r", message.fader_r);
+                }
+
+                if (previous.cross_fader != message.cross_fader) {
+                  changes[13] =
+                      std::make_tuple("/cross_fader", message.cross_fader);
+                }
+
+                auto spa_data = buffer->get_spa_data(0);
+                if (std::any_of(changes.begin(), changes.end(),
+                                [](const auto &change) {
+                                  return change.has_value();
+                                })) {
+                  struct spa_pod_builder builder;
+                  struct spa_pod_frame frame;
+                  spa_pod_builder_init(&builder, spa_data->data,
+                                       spa_data->maxsize);
+                  spa_pod_builder_push_sequence(&builder, &frame, 0);
+                  spa_pod_builder_control(&builder, 0, SPA_CONTROL_OSC);
+
+                  char osc_buffer[4096];
+                  for (auto &&change :
+                       changes | std::views::filter([](const auto &change) {
+                         return change.has_value();
+                       })) {
+                    OSCPP::Client::Packet packet(osc_buffer, 4096);
+
+                    auto [path, value] = change.value();
+                    std::cout << "sending: " << path << " " << value
+                              << std::endl;
+                    packet.openMessage(path.c_str(), 1)
+                        .float32(value)
+                        .closeMessage();
+                    auto size = packet.size();
+                    spa_pod_builder_bytes(&builder, osc_buffer, size);
+                  }
+
+                  auto pod = static_cast<spa_pod *>(
+                      spa_pod_builder_pop(&builder, &frame));
+
+                  spa_data->chunk->size = builder.state.offset;
+                }
+
+                previous = message;
+              }
+            }
+          }
+          buffer.value().finish();
+        }
+      });
+
+  std::thread thread([&device, &queue]() {
+    std::ifstream file(device, std::ios::binary);
+
+    if (!file.is_open()) {
+      std::cerr << "Unable to open device" << std::endl;
+      return 1;
+    }
+
+    std::array<std::uint8_t, 30> buffer = {0};
+    traktor_z1_message report;
+
+    while (file.read(reinterpret_cast<char *>(buffer.data()), buffer.size())) {
+      if (buffer[0] == 1) {
+        if (file.gcount() == 30) {
+          report.gain_l = (buffer[2] << 8) + buffer[1];
+          report.hi_l = (buffer[4] << 8) + buffer[3];
+          report.mid_l = (buffer[6] << 8) + buffer[5];
+          report.low_l = (buffer[8] << 8) + buffer[7];
+          report.filter_l = (buffer[10] << 8) + buffer[9];
+          report.gain_r = (buffer[12] << 8) + buffer[11];
+          report.hi_r = (buffer[14] << 8) + buffer[13];
+          report.mid_r = (buffer[16] << 8) + buffer[15];
+          report.low_r = (buffer[18] << 8) + buffer[17];
+          report.filter_r = (buffer[20] << 8) + buffer[19];
+          report.cue_mix = (buffer[22] << 8) + buffer[21];
+          report.fader_l = (buffer[24] << 8) + buffer[23];
+          report.fader_r = (buffer[26] << 8) + buffer[25];
+          report.cross_fader = (buffer[28] << 8) + buffer[27];
+          queue.push(report);
+        } else {
+          std::cout << "report had unexpected length" << std::endl;
+        }
+      }
+    }
+
+    file.close();
+    return 0;
+  });
+
+  auto app = builder.build();
+  if (app.has_value()) {
+    app.value()->run();
+  }
+
+  return 0;
+}
