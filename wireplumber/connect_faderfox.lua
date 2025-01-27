9ebebc565b98106694911b83541385ccd730b6c1
Frank Krick <frank.krick@gmail.com>
2025-01-27 17:26:13 -0500

    Connect faderfox midi controller

---
+++ wireplumber/connect_faderfox.lua
@@ -0,0 +1,71 @@
+source_port_om = ObjectManager({
+	Interest({
+		type = "port",
+		Constraint({
+			"port.alias",
+			"=",
+			"Faderfox PC4:Faderfox PC4 MIDI 1",
+		}),
+		Constraint({
+			"port.direction",
+			"=",
+			"out",
+		}),
+	}),
+})
+
+target_port_om = ObjectManager({
+	Interest({
+		type = "port",
+		Constraint({
+			"port.alias",
+			"=",
+			"pmx-midi-router:input channels",
+		}),
+		Constraint({
+			"port.direction",
+			"=",
+			"in",
+		}),
+	}),
+})
+
+source_port_om:activate()
+target_port_om:activate()
+
+MyLink = nil
+local function connect()
+	if MyLink == nil then
+		for source_port in source_port_om:iterate() do
+			for target_port in target_port_om:iterate() do
+				print("output port id", source_port.properties["port.id"])
+				print("input port id", target_port.properties["port.id"])
+				print("output node id", source_port.properties["node.id"])
+				print("input node id", target_port.properties["node.id"])
+				MyLink = Link("link-factory", {
+					["link.output.port"] = source_port.properties["port.id"],
+					["link.input.port"] = target_port.properties["port.id"],
+					["link.output.node"] = source_port.properties["node.id"],
+					["link.input.node"] = target_port.properties["node.id"],
+				})
+				MyLink:activate(1)
+			end
+		end
+	end
+end
+
+source_port_om:connect("installed", function(om)
+	connect()
+end)
+
+target_port_om:connect("installed", function(om)
+	connect()
+end)
+
+source_port_om:connect("object-added", function(om)
+	connect()
+end)
+
+target_port_om:connect("object-added", function(om)
+	connect()
+end)
