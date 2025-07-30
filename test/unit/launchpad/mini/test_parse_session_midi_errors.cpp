4e6256842fcbffb13ef24e3df2e17f8b5ae583db
Frank Krick <frank.krick@gmail.com>
2025-07-29 20:52:47 -0400

    add additional invalid index

--- test/unit/launchpad/mini/test_parse_session_midi_errors.cpp
+++ test/unit/launchpad/mini/test_parse_session_midi_errors.cpp
@@ -26,4 +26,6 @@ TEST_P(InvalidButtonIndex, NoteOnV2) {
 
 INSTANTIATE_TEST_SUITE_P(SessionModeErrors, InvalidButtonIndex,
                          testing::Values(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100,
-                           101, 102, 103, 104));
+                           101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111
+                           , 112, 113, 114, 115, 116, 117, 118, 119, 120, 121,
+                           122, 123, 124, 125, 126, 127));
