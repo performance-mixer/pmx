dd0badc40383d7f86b3af71fe23ece129d7104b6
Frank Krick <frank.krick@gmail.com>
2025-08-01 17:51:20 -0400

    launchpad mini session button event parsing

--- test/unit/launchpad/mini/test_parse_session_midi_buttons_errors.cpp
+++ test/unit/launchpad/mini/test_parse_session_midi_buttons_errors.cpp
@@ -4,6 +4,12 @@
 #include <ump/messages.h>
 #include <ump/note.h>
 
+
+/*
+ * TODO:
+ * These tests needs to be more complicated, top row and all buttons in the last
+ * column use CC messages to indicate changes
+ */
 class InvalidButtonIndex : public testing::TestWithParam<unsigned int> {};
 
 TEST_P(InvalidButtonIndex, NoteOnV2) {
@@ -76,7 +82,24 @@ TEST_P(InvalidButtonIndex, NoneOffV1) {
 }
 
 INSTANTIATE_TEST_SUITE_P(SessionModeErrors, InvalidButtonIndex,
-                         testing::Values(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100,
-                           101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111
-                           , 112, 113, 114, 115, 116, 117, 118, 119, 120, 121,
-                           122, 123, 124, 125, 126, 127));
+                         testing::Values(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 19,
+                           20, 29, 30, 39, 40, 49, 50 , 59, 60, 69, 70, 79, 80,
+                           89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101
+                           , 102, 103, 104, 105, 106, 107, 108, 109, 110, 111,
+                           112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122
+                           , 123, 124, 125, 126, 127));
+
+class InvalidControlChangeIndex : public testing::TestWithParam<unsigned int> {
+};
+
+INSTANTIATE_TEST_SUITE_P(SessionModeErrors, InvalidControlChangeIndex,
+                         testing::Values(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+                           12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 23, 24, 25,
+                           26, 27, 28, 30, 31, 32, 33, 34, 35, 36, 37, 38, 40,
+                           41, 42, 43, 44, 45, 46, 47, 48, 50, 51, 52, 53, 54,
+                           55, 56, 57, 58, 60, 61, 62, 63, 64, 65, 66, 67, 68,
+                           70, 71, 72, 73, 74, 75, 76, 77, 78, 80, 81, 82, 83,
+                           84, 85, 86, 87, 88, 90, 100, 101, 102, 103, 104, 105,
+                           106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116
+                           , 117, 118, 119, 120, 121, 122, 123, 124, 125, 126,
+                           127));
---
+++ test/unit/launchpad/mini/test_parse_session_midi_faders.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+#include <slp_ctrl/launchpad/mini/parse_session_midi.h>
+#include <ump/messages.h>
+#include <ump/note.h>
